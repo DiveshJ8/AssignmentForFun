8c21fc9b40193d1a42f968a98888ecb3555a4397
DiveshJ8 <91094159+DiveshJ8@users.noreply.github.com>
2022-12-01 00:06:16 -0500

    Arraylists updated

--- src/main/java/Collections/ArrayLists.java
+++ src/main/java/Collections/ArrayLists.java
@@ -5,18 +5,36 @@ import java.util.ArrayList;
 public class ArrayLists {
 
     public ArrayList<Integer> addShit(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        return null;
+    	
+    	for(Integer i : list2) {
+    		list1.add(i);
+    	}
+        return list1;
     }
 
     public Integer addShitTogether(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        return null;
+    	
+    	int sum = 0;
+    	for(Integer i : list1) {
+    		sum += i.intValue();
+    	}
+    	
+    	for(Integer i : list2) {
+    		sum += i.intValue();
+    	}
+        return sum;
     }
 
     public ArrayList<Integer> removeAll(ArrayList<Integer> original, Integer toRemove) {
-        return null;
+    	
+    	original.remove(toRemove);
+        return original;
     }
 
     public boolean happyList(ArrayList<String> original) {
+    	
+    	
+    	
         return false;
     }
 }
